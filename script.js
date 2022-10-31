1497c9de8caac2c57d1356973deaf728bf804b90
Eviker <80265492+Eviker@users.noreply.github.com>
2022-10-30 20:46:39 -0400

    Update script.js
    everything needs to be more efficient but it works so i dont care

--- script.js
+++ script.js
@@ -8,18 +8,35 @@ Bombs = 99;
 
 board = Array(Width).fill(0).map(x => Array(Height).fill(0))
 
+gameTypes = {
+    "beginner": {
+        W: 9,
+        H: 9,
+        B: 10
+    },
+    "intermediate": {
+        W: 16,
+        H: 16,
+        B: 40
+    },
+    "expert": {
+        W: 30,
+        H: 16,
+        B: 99
+    }
+}
+
 FirstClick = true;
 GameOver = false;
 
 for( i = 0; i < Width; i++){
     for( j = 0; j < Height; j++){        
-        tempTile = document.createElement("div");
+        var tempTile = document.createElement("div");
         tempTile.className = "tile";
         tempTile.style.position = "absolute";
         tempTile.style.left = 25 * i;
         tempTile.style.top = 25 * j;
-        tempTile.onclick = click;
-        tempTile.oncontextmenu = flag;
+        tempTile.onmousedown = click;
         tempTile.x = i;
         tempTile.y = j;
         tempTile.num = 0;
@@ -32,22 +49,22 @@ for( i = 0; i < Width; i++){
     }
 }
 
-
-function isAdjacent(x1,y1,x2,y2){
-    xDif = Math.abs(x1-x2);
-    yDif = Math.abs(y1-y2);
-    if( (xDif == 1 || xDif == 0) && (yDif == 1 || yDif == 0)){
-        return true
-    }
-    return false
+function customGame(width,height,bombNum){
+    Width = width;
+    Height = height;
+    Bombs = bombNum;
+    newGame("custom");
 }
 
 function createBomb(x,y){
-    bombsLeft = Bombs;
+    
+    var bombsLeft = Bombs;
+
     while( bombsLeft > 0){
         RanX = Math.floor(Math.random() * Width);
         RanY = Math.floor(Math.random() * Height);
-        if( !board[RanX][RanY].bomb && !(isAdjacent(x,y,RanX,RanY)) ){
+
+        if( !board[RanX][RanY].bomb && !((Math.abs(RanX-x) + Math.abs(RanY-y)) <= 2) ){
             board[RanX][RanY].bomb = true;
             bombsLeft -= 1;
         }
@@ -70,7 +87,6 @@ function createNumbers(){
     }
 }
 
-
 function isAdjacentToGray(x,y){
     for( a = x - 1; a <= x + 1; a++){
         for( b = y - 1; b <= y + 1; b++){
@@ -84,51 +100,8 @@ function isAdjacentToGray(x,y){
     return false
 }
 
-function flagRemaining(){
-    for( i = 0; i < Width; i++){
-        for( j = 0; j < Height; j++){
-            if(!board[i][j].clicked){
-                board[i][j].innerHTML = "\u{1F6A9}";
-            }
-        }
-    }
-    console.log("hello");
-}
-
-function checkWin(){
-    counter = 0;
-    counter2 = 0;
-    for( i = 0; i < Width; i++){
-        for( j = 0; j < Height; j++){
-            if(board[i][j].bomb && board[i][j].flag){
-                counter++;
-            }
-            if(!board[i][j].clicked){
-                counter2++;
-            }
-        }
-    }
-    if (counter == Bombs || counter2 == Bombs){
-        if(counter2 == Bombs){ flagRemaining() }
-        alert("You won!");
-        GameOver = true;
-    }
-}
-
-function clearTile(x,y){
-    try{
-        tile = board[x][y];
-        tile.style.backgroundColor = "lightgrey";
-        tile.clicked = true;
-        tile.style.color = TextColor[tile.num];
-        tile.innerHTML = (tile.bomb) ? "\u{1F4A3}" : ((tile.num == 0) ? " " : tile.num);
-        
-        
-    } catch(err){}
-}
-    
 function clearZeroes(){
-    toggle = true;
+    var toggle = true;
     while( toggle ){
         toggle = false;
         for( i = 0; i < Width; i++){
@@ -153,61 +126,130 @@ function clearZeroes(){
             }
         }
     }  
-}    
+} 
 
-function click(){
-    if(FirstClick){
-        createBomb(this.x,this.y);
-        createNumbers();
-        FirstClick = false;
-    }
-  
-    if( !this.flag && !GameOver ){
-        clearTile(this.x,this.y);
+function click(e){
+    if( !GameOver ){
+        if( e.which == 1){
+            if(FirstClick){
+                createBomb(this.x,this.y);
+                createNumbers();
+                FirstClick = false;
+            }
+        
+            if( !this.flag ){
+                clearTile(this.x,this.y);
 
-        if( this.num == 0) clearZeroes();   
-        if( this.bomb == true) GameOver = true;    
-        if( !this.bomb) checkWin();
+                if(this.num == 0 ) clearZeroes();   
+                if(this.bomb)  GameOver = true; 
+            }
+        } else if( e.which == 2){
+
+            middleClick(this.x,this.y);
+
+        } else if( e.which == 3){
+           flag(this.x,this.y);
+        } 
+
+        checkGame();
     }
-   
-    
 }
 
-function flag(){
-    if( !this.clicked && !GameOver){
-        if(this.flag){
-            this.innerHTML = ""
-        } else {
-            this.innerHTML = "\u{1F6A9}"
-        }
-        this.flag = !this.flag;
+function clearTile(x,y){
+    try{
+        var tile = board[x][y];
+        tile.style.backgroundColor = "lightgrey";
+        tile.clicked = true;
+        tile.style.color = TextColor[tile.num];
+        tile.innerHTML = tile.bomb ? "\u{1F4A3}" : ((tile.num == 0) ? " " : tile.num);
+        if(tile.bomb) GameOver = true;
+    } catch(err){}
+}
 
-        checkWin();
+function middleClick(x,y){
+    var counter = 0;
+    for( i = x - 1; i <= x + 1; i++){
+        for( j = y - 1; j <= y + 1; j++){
+            try{
+                if(board[i][j].flag){
+                    counter++;
+                }
+            }catch(err){}
+        }
     }
-   
+    if( counter == board[x][y].num){
+        for( i = -1; i < 2; i++){
+            for( j = -1; j < 2; j++){
+                try {
+                    if( !board[x+i][y+j].flag && !(i==0 && j==0)) clearTile(x+i,y+j);
+                } catch(err){}
+            }
+        }
+    }
+    clearZeroes();
+} 
+
+function flag(x,y){
+    var tile = board[x][y];
+
+    if( !tile.clicked ){
+        tile.innerHTML = tile.flag ? "" : "\u{1F6A9}";
+        tile.flag = !tile.flag;
+    }  
+} 
+
+function checkGame(){
+    if( GameOver == false){
+        var counter = 0;
+        var counter2 = 0;
+        for( i = 0; i < Width; i++){
+            for( j = 0; j < Height; j++){
+                if(board[i][j].bomb && board[i][j].flag){
+                    counter++;
+                }
+                if(!board[i][j].clicked){
+                    counter2++;
+                }
+            }
+        }
+        if (counter == Bombs || counter2 == Bombs){
+            clearRemaining();
+            alert("You won!");
+            GameOver = true;
+        } 
+    } else {
+        showRemaining();
+    }
+}
+
+function clearRemaining(){
+    for( i = 0; i < Width; i++){
+        for( j = 0; j < Height; j++){
+            var tile = board[i][j];
+           
+            tile.bomb && !tile.clicked ? tile.innerHTML = "\u{1F6A9}" : clearTile(i,j);   
+        }
+    }
+}
+
+function showRemaining(){
+    for( i = 0; i < Width; i++){
+        for( j = 0; j < Height; j++){
+            var tile = board[i][j];
+            if (tile.bomb && !tile.flag) tile.innerHTML = "\u{1F4A3}";
+        }
+    }
 }
 
 function newGame(style){
-    switch(style){
-        case "b": 
-            Width = 9;
-            Height = 9;
-            Bombs = 10;
-            break;
-        case "i": 
-            Width = 16;
-            Height = 16;
-            Bombs = 40;
-            break;
-        case "e": 
-            Width = 30;
-            Height = 16;
-            Bombs = 99;
-            break;
-        default:
-            Width = 10;
-            Height = 10;
-            Bombs = 5;
+    if(style != "custom"){
+        Width = gameTypes[style].W;
+        Height = gameTypes[style].H;
+        Bombs = gameTypes[style].B;
+    } 
+    
+    if( Bombs > (Width*Height*0.8)){
+        Bombs = Width*Height*0.8;
     }
 
     board = Array(Width).fill(0).map(x => Array(Height).fill(0))
@@ -219,13 +261,12 @@ function newGame(style){
 
     for( i = 0; i < Width; i++){
         for( j = 0; j < Height; j++){        
-            tempTile = document.createElement("div");
+            var tempTile = document.createElement("div");
             tempTile.className = "tile";
             tempTile.style.position = "absolute";
             tempTile.style.left = 25 * i;
             tempTile.style.top = 25* j;
-            tempTile.onclick = click;
-            tempTile.oncontextmenu = flag;
+            tempTile.onmousedown = click;
             tempTile.x = i;
             tempTile.y = j;
             tempTile.num = 0;
@@ -237,11 +278,4 @@ function newGame(style){
             document.getElementById("myDiv").appendChild( tempTile );
         }
     }
-    
-
 }
-
-
-
-
-
